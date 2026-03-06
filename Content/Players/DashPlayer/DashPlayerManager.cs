b920d88356e21e103ad0e85cdd70a54e472fa972
DemonMarisa <121868896@qq.com>
2026-03-06 22:25:15 +0800

    拉取一下，准备冲刺系统搬运

---
+++ Content/Players/DashPlayer/DashPlayerManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace Moreplugins.Content.Players.DashPlayer
+{
+    public class DashPlayerManager : ModPlayer
+    {
+        public static List<MPPlayerDash> DashCollection = [];
+    }
+}
